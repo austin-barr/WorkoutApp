681df083b789284d491c8df9ae18942b7f61e056
austin-barr <abarr2118@gmail.com>
2023-10-24 22:06:59 -0400

    added draft of home page front end

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
+// App.js
 import './App.css';
 import LoginPage from './LoginPage';
+import HomePage from './HomePage';
 
-// App.js
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 function App() {
   return (
-     // <div className="App">
-       //     <LoginPage />
-         //   </div>
 
-    
     <div className="App">
       <BrowserRouter>
       <Routes>
         <Route path="/" element = {<LoginPage />} />
-        </Routes>
+        <Route path="/home/" element = {<HomePage />} />
+      </Routes>
       </BrowserRouter>
       </div>
     
